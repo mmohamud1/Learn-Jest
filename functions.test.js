27b7c6454758cf2aa3bfd1bb1aeaa8188506838a
Mohamed Mohamud <m.mohamud1@hotmail.com>
2021-01-11 14:09:31 +0000

    add second test

--- functions.test.js
+++ functions.test.js
@@ -1,5 +1,12 @@
 const functions = require('./functions');
 
+
+// Test To find expected outcome
 test('Adds 2 + 2 to equal 4', () => {
     expect(functions.add(2, 2)).toBe(4)
-})
+});
+
+// Test to make sure outcome does not not occur
+test('Adds 2 + 2 to NOT equal 5', () => {
+    expect(functions.add(2, 2)).not.toBe(5)
+});

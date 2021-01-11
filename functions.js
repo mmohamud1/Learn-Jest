41da738acf0417334541aec0bdce2b7e4c816816
Mohamed Mohamud <m.mohamud1@hotmail.com>
2021-01-11 14:15:56 +0000

    add new function and test for null

--- functions.js
+++ functions.js
@@ -1,5 +1,7 @@
+// Objects with functions to test
 const functions = {
-    add: (num1, num2) => num1 + num2
+    add: (num1, num2) => num1 + num2,
+    isNull: () => null
 }
 
 module.exports = functions;
--- functions.test.js
+++ functions.test.js
@@ -10,3 +10,15 @@ test('Adds 2 + 2 to equal 4', () => {
 test('Adds 2 + 2 to NOT equal 5', () => {
     expect(functions.add(2, 2)).not.toBe(5)
 });
+
+// PARAMETERS TO CHECK FOR TRUTHY AND FALSEY VALUES
+// toBeNull matches only null
+// toBeUndefined matches only undefined
+// toBeUndefined opposite of toBeUndefined
+// toBeTruthy matched anything that an if statement treats as true
+// toBeFalsey matched anything that an if statement treats as false
+
+// Some examples below
+test('should be null', () => {
+    expect(functions.isNull()).toBeNull()
+});
